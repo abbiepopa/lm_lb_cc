3f9f0552f55592d5c3be8167a66e034e121dc13c
Abbie Popa <ampopa@ucdavis.edu>
2017-05-08 10:45:37 -0700

    line bisection comparison

--- behavior.R
+++ behavior.R
@@ -23,3 +23,26 @@ dx2er <- function(s){
 }
 
 d$dx2 <- unlist(lapply(d$dx, dx2er))
+
+###Line Bisection###
+##SCA separate##
+#right hand#
+pairwise.t.test(x = d$RMEAN,g = d$dx, p.adjust.method = 'none')
+#left hand#
+pairwise.t.test(x = d$LMEAN,g = d$dx, p.adjust.method = 'none')
+#both hands#
+pairwise.t.test(x = d$ALLMEAN,g = d$dx, p.adjust.method = 'none')
+
+##SCA combined##
+#right hand#
+pairwise.t.test(x = d$RMEAN,g = d$dx2, p.adjust.method = 'none')
+#left hand#
+pairwise.t.test(x = d$LMEAN,g = d$dx2, p.adjust.method = 'none')
+#both hands#
+pairwise.t.test(x = d$ALLMEAN,g = d$dx2, p.adjust.method = 'none')
+
+#follow-up for right hand 22q vs td since that is what's significant#
+t.test(d[which(d$dx == "q22"),"RMEAN"], d[which(d$dx == "1td"),"RMEAN"])
+
+library(psych)
+(describeBy(d$RMEAN, d$dx)$`1td`[['mean']] - describeBy(d$RMEAN, d$dx)$`q22`[['mean']])/describe(d$RMEAN)[['sd']]
