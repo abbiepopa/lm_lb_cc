1b9146320d783f090e69d054b79563ff7636aa1c
Abbie Popa <ampopa@ucdavis.edu>
2017-05-13 12:30:37 -0700

    separating out line bisection for easier reading

---
+++ CCC_WM_norm_lbonly.R
@@ -0,0 +1,103 @@
+setwd("~/Documents/cycle2/linemark/data/processed")
+
+lb<-read.csv("lb.csv")
+seg<-read.csv("seg.csv")
+
+#lb need studyid, RMEAN, LMEAN, ALLMEAN, Gender, Dx, AGEMONTHS
+lbs<-lb[,c("StudyID","Gender", "Dx","AGEMONTHS","RMEAN","LMEAN","ALLMEAN")]
+colnames(lbs)<-tolower(colnames(lbs))
+
+#seg needs Measure.volume (that's actually studyid), CC_Posterior, CC_Mid_Posterior, CC_Central, CC_Mid_Anterior, CC_Anterior, CorticalWhiteMatterVol
+segs<-seg[,c("Measure.volume","DX","CC_Posterior","CC_Mid_Posterior","CC_Central","CC_Mid_Anterior","CC_Anterior", "CorticalWhiteMatterVol")]
+colnames(segs)<-tolower(colnames(segs))
+
+segs_notnorm<-segs
+
+segs$cc_posterior <- segs$cc_posterior/segs$corticalwhitemattervol
+segs$cc_mid_posterior <- segs$cc_mid_posterior/segs$corticalwhitemattervol
+segs$cc_central <- segs$cc_central/segs$corticalwhitemattervol
+segs$cc_mid_anterior <- segs$cc_mid_anterior/segs$corticalwhitemattervol
+segs$cc_anterior <- segs$cc_anterior/segs$corticalwhitemattervol
+
+
+colnames(segs)[[1]]<-"studyid"
+lb_seg<-merge(lbs, segs, by="studyid")
+
+#or should we have two variables? left error and right error?
+lb_seg$err_to_lft <- 0
+lb_seg[which(lb_seg$allmean <0), "err_to_lft"]<- lb_seg[which(lb_seg$allmean < 0), "allmean"]
+lb_seg$err_to_rgt <- 0
+lb_seg[which(lb_seg$allmean > 0), "err_to_rgt"] <- lb_seg[which(lb_seg$allmean > 0), "allmean"]
+
+#lft err, errors to the right are set to zero
+llrfit_posterior_lb<-lm(err_to_lft~cc_posterior, data=lb_seg)
+llrfit_mid_posterior_lb<-lm(err_to_lft~cc_mid_posterior, data=lb_seg)
+llrfit_central_lb<-lm(err_to_lft~cc_central, data=lb_seg)
+llrfit_mid_anterior_lb<-lm(err_to_lft~cc_mid_anterior, data=lb_seg)
+llrfit_anterior_lb<-lm(err_to_lft~cc_anterior, data=lb_seg)
+
+#rgt err, errors to the left are set to zero
+rlrfit_posterior_lb<-lm(err_to_rgt ~cc_posterior, data=lb_seg)
+rlrfit_mid_posterior_lb<-lm(err_to_rgt ~cc_mid_posterior, data=lb_seg)
+rlrfit_central_lb<-lm(err_to_rgt ~cc_central, data=lb_seg)
+rlrfit_mid_anterior_lb<-lm(err_to_rgt ~cc_mid_anterior, data=lb_seg)
+rlrfit_anterior_lb<-lm(err_to_rgt ~cc_anterior, data=lb_seg)
+
+#print results
+summary(llrfit_posterior_lb)
+summary(llrfit_mid_posterior_lb)
+summary(llrfit_central_lb)
+summary(llrfit_mid_anterior_lb)
+summary(llrfit_anterior_lb)
+
+p.adjust(c(0.0111, 0.00513, 0.0477, 0.319, 0.0000119), method = 'fdr')
+
+summary(rlrfit_posterior_lb)
+summary(rlrfit_mid_posterior_lb)
+summary(rlrfit_central_lb)
+summary(rlrfit_mid_anterior_lb)
+summary(rlrfit_anterior_lb)
+
+
+#lb by dx
+summary(lm(err_to_lft ~ cc_posterior, data = lb_seg[which(lb_seg$dx.x == "22q"),]))
+summary(lm(err_to_lft ~ cc_mid_posterior, data = lb_seg[which(lb_seg$dx.x == "22q"),]))
+summary(lm(err_to_lft ~ cc_central, data = lb_seg[which(lb_seg$dx.x == "22q"),]))
+summary(lm(err_to_lft ~ cc_mid_anterior, data = lb_seg[which(lb_seg$dx.x == "22q"),]))
+summary(lm(err_to_lft ~ cc_anterior, data = lb_seg[which(lb_seg$dx.x == "22q"),]))
+p.adjust(c(0.018, 0.130, 0.117, 0.0323, 0.136), method = 'fdr')
+
+summary(lm(err_to_lft ~ cc_posterior, data = lb_seg[which(lb_seg$dx.x == "TD"),]))
+summary(lm(err_to_lft ~ cc_mid_posterior, data = lb_seg[which(lb_seg$dx.x == "TD"),]))
+summary(lm(err_to_lft ~ cc_central, data = lb_seg[which(lb_seg$dx.x == "TD"),]))
+summary(lm(err_to_lft ~ cc_mid_anterior, data = lb_seg[which(lb_seg$dx.x == "TD"),]))
+summary(lm(err_to_lft ~ cc_anterior, data = lb_seg[which(lb_seg$dx.x == "TD"),]))
+
+summary(lm(err_to_rgt ~ cc_posterior, data = lb_seg[which(lb_seg$dx.x == "22q"),]))
+summary(lm(err_to_rgt ~ cc_mid_posterior, data = lb_seg[which(lb_seg$dx.x == "22q"),]))
+summary(lm(err_to_rgt ~ cc_central, data = lb_seg[which(lb_seg$dx.x == "22q"),]))
+summary(lm(err_to_rgt ~ cc_mid_anterior, data = lb_seg[which(lb_seg$dx.x == "22q"),]))
+summary(lm(err_to_rgt ~ cc_anterior, data = lb_seg[which(lb_seg$dx.x == "22q"),]))
+
+summary(lm(err_to_rgt ~ cc_posterior, data = lb_seg[which(lb_seg$dx.x == "TD"),]))
+summary(lm(err_to_rgt ~ cc_mid_posterior, data = lb_seg[which(lb_seg$dx.x == "TD"),]))
+summary(lm(err_to_rgt ~ cc_central, data = lb_seg[which(lb_seg$dx.x == "TD"),]))
+summary(lm(err_to_rgt ~ cc_mid_anterior, data = lb_seg[which(lb_seg$dx.x == "TD"),]))
+summary(lm(err_to_rgt ~ cc_anterior, data = lb_seg[which(lb_seg$dx.x == "TD"),]))
+
+summary(lm(err_to_lft ~ cc_posterior, data = lb_seg[which(lb_seg$dx.y == 3),]))
+summary(lm(err_to_lft ~ cc_mid_posterior, data = lb_seg[which(lb_seg$dx.y == 3),]))
+summary(lm(err_to_lft ~ cc_central, data = lb_seg[which(lb_seg$dx.y == 3),]))
+summary(lm(err_to_lft ~ cc_mid_anterior, data = lb_seg[which(lb_seg$dx.y == 3),]))
+summary(lm(err_to_lft ~ cc_anterior, data = lb_seg[which(lb_seg$dx.y == 3),]))
+#only anterior significant
+p.adjust(0.0217, method = 'fdr', n = 5) #does not survive
+
+summary(lm(err_to_rgt ~ cc_posterior, data = lb_seg[which(lb_seg$dx.y == 3),]))
+summary(lm(err_to_rgt ~ cc_mid_posterior, data = lb_seg[which(lb_seg$dx.y == 3),]))
+summary(lm(err_to_rgt ~ cc_central, data = lb_seg[which(lb_seg$dx.y == 3),]))
+summary(lm(err_to_rgt ~ cc_mid_anterior, data = lb_seg[which(lb_seg$dx.y == 3),]))
+summary(lm(err_to_rgt ~ cc_anterior, data = lb_seg[which(lb_seg$dx.y == 3),]))
+#anterior and mid-anterior significant
+p.adjust(c(0.0276, 0.00026), method = 'fdr', n = 5)
+#anterior does not survive, mid-anterior does
